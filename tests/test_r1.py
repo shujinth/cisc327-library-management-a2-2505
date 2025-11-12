42614891f3f366e504812b12f5dc4b321b2d427d
Shujinth <shujinth.sasi@gmail.com>
2025-11-12 10:50:29 -0500

    updating database

--- tests/test_r1.py
+++ tests/test_r1.py
@@ -35,6 +35,20 @@ def test_add_book_empty_author():
     assert success == False
     assert "required" in message.lower()
 
+def test_add_book_author_name_too_long():
+    """Test adding a book with invalid author name. """
+    success, message = add_book_to_catalog("Test Book", "eejvfndijpvndsdufsdufbvdbvsdfubvdubfdpsdufbvpsdofuvbspdoufbvspdvspdfuvpsdofuvbsdiufvbdpsfuvbsdpfuvbspdofuberjnvuprubwqornovier", "1234567890123", 5)
+
+    assert success == False
+    assert "less than 100" in message.lower()
+
+def test_add_book_name_too_long():
+    """Test adding a book with invalid author name. """
+    success, message = add_book_to_catalog("eejvfndijpvndsdufsdufbvdbvsdfubvdubfdpsdufbvpsdofuvbspdoufbvspdvspdfuvpsdofuvbsdiufvbdpsfuvbsdpfuvbspdofuberjnvuprubwqornoviereejvfndijpvndsdufsdufbvdbvsdfubvdubfdpsdufbvpsdofuvbspdoufbvspdvspdfuvpsdofuvbsdiufvbdpsfuvbsdpfuvbspdofuberjnvuprubwqornoviereejvfndijpvndsdufsdufbvdbvsdfubvdubfdpsdufbvpsdofuvbspdoufbvspdvspdfuvpsdofuvbsdiufvbdpsfuvbsdpfuvbspdofuberjnvuprubwqornovier", "Test Author", "1234567890123", 5)
+
+    assert success == False
+    assert "less than 200" in message.lower()
+
 def test_add_book_invalid_total_copies():
     """Test adding a book with zero or negative total copies."""
     success, message = add_book_to_catalog("Test Book", "Test Author", "1234567890123", 0)
